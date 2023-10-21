83678d3841cfac61806d28f7e7f6015d22b6d85c
Neil Harvey Batu <115128553+NeilH11@users.noreply.github.com>
2023-10-21 10:33:28 +0800

    Simple Calculator

---
+++ Personal Projects/Calculator/Calculator.js
@@ -0,0 +1,17 @@
+function appendToDisplay(value) {
+  document.calculator.display.value += value;
+}
+
+function clearDisplay() {
+  document.calculator.display.value = "";
+}
+
+function calculateResult() {
+  try {
+    document.calculator.display.value = eval(
+      document.calculator.display.value
+    );
+  } catch (error) {
+    document.calculator.display.value = "Error";
+  }
+}
